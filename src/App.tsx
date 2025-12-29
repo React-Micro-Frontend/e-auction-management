a87c2a3a04abd9047cf851d4f989b8c882a5f564
Muhammad Shoaib <muhammad.shoaib@psw.gov.pk>
2025-12-29 12:33:24 +0500

    added directory structuer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Card, PageHeader, StatCard, Button } from "customMain/components/shared
 import { useAppDispatch, useAppSelector } from "customMain/store/hooks";
 import { increment, decrement, reset } from "customMain/store/slices/counterSlice";
 import { addUser } from "customMain/store/slices/userSlice";
+import { formatRelativeTime, formatDate, formatCurrency } from "customMain/utils/dateHelpers";
 import { mockAuctions, mockActivities } from "./data/mockData";
-import { formatRelativeTime, formatDate, formatCurrency } from "./utils";
 
 export default function App() {
   const dispatch = useAppDispatch();
